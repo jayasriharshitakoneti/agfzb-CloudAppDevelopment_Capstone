b88fac6ce20bd926c3ddd09758773023b347048c
Koneti Jaya Sriharshita <harshitakoneti@gmail.com>
2021-10-08 13:05:47 +0000

    week 2

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -14,11 +14,9 @@ urlpatterns = [
 
     # path for contact us view
     path(route='contact/',view=views.contact,name='contact'),
-    # path for registration
-
-    # path for login
-
-    # path for logout
+    path('registration/', views.registration_request, name='registration'),
+    path('login/', views.login_request, name='login'),
+    path('logout/', views.logout_request, name='logout'),
 
     path(route='', view=views.get_dealerships, name='index'),
 
